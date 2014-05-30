6fec2d619e1ee5cd48ca077da189b71a36a81f9b
Larry Staton Jr <larry@sweetpeasoftware.com>
2014-05-29 20:17:42 -0400

    Minified build of app in build directory

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ requirejs.config({
     'amber_core': 'bower_components/amber/src',
     'amber_lib': 'bower_components',
     'jquery': 'bower_components/jquery/jquery.min',
-    'director': 'bower_components/director/build/director.min'
+    'director': 'bower_components/director/build'
   }
 });
 
@@ -17,7 +17,8 @@ requirejs([
     'Examples/Examples-ToDo',
     'Examples/Examples-ToDo-Models',
     'Examples/Examples-ToDo-Views',
-    'Examples/Examples-ToDo-Announcements'
+    'Examples/Examples-ToDo-Announcements',
+    'director/director.min'
   ],
   function (_, smalltalk) {
     console.log(smalltalk);
